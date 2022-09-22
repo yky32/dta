07fec9107632f6050adf8fa0f9a7851824f2660c
Wayne Yu <wayneyu.dev@gmail.com>
2022-09-22 15:27:04 +0800

    Center

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,27 +11,16 @@ const Home: NextPage = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-
-    <main>
-
-      <div style={{ width: '250px', height: '250px' }}>
-        <GameCard 
-          title={"123"} tags={['123', '456']} gameImageUrl={'https://cdn.discordapp.com/attachments/1022347629344063581/1022347699422503003/d681acd50b03a07815103f41543abba8.jpeg'} />
+    <main className='flex'>
+      <div className='mx-auto'>
+        <div style={{width:'250px', height:'250px'}}>
+          <GameCard title={''} gameImageUrl={'https://cdn.discordapp.com/attachments/1022347629344063581/1022347699422503003/d681acd50b03a07815103f41543abba8.jpeg'} tags={['123']}></GameCard>
+        </div>
       </div>
     </main>
 
 
     <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
     </footer>
   </div>
 )
