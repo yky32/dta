d76ffc4dfbfe494418d7cbf1cac980058332cc54
Wayne Yu <wayneyu.dev@gmail.com>
2022-09-27 13:44:45 +0800

    Add

--- pages/games/[id].tsx
+++ pages/games/[id].tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import requests from "../../utils/requests"
 import { Game } from "../../typings"
 import InGameCard from "../../components/cards/inGameCard"
+import { BsPlusLg } from "react-icons/bs"
 
 interface Props {
   game: Game
@@ -15,15 +16,22 @@ const InGame = ({ game }: Props) => {
   return (
     <div className='space-y-4 layout'>
       <div className='text-2xl'>{gameId}</div>
-      
+
       <div className="grid 
         grid-cols-1 gap-4
         md:grid-cols-2 md:gap-8
         ">
-          <InGameCard
-            game={game}
-          />
+        <InGameCard
+          game={game}
+        />
+
+        <div className="h-52 rounded-lg border-dotted border-2 border-secondary-2 flex items-center justify-center">
+          <div className=" space-y-2">
+            <div className="text-3xl text-secondary-1"><BsPlusLg className="mx-auto" /></div>
+            <div className="text-2xl text-secondary-1">Add/開局</div>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
